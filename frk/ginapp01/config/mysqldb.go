a7fbebf6c65e7a80e5ad6c2ad6760bcb0970a666
sevenyoungairye <lel.ng.top@gmail.com>
2022-05-09 16:09:22 +0800

    sql log.

--- frk/ginapp01/config/mysqldb.go
+++ frk/ginapp01/config/mysqldb.go
@@ -3,12 +3,27 @@ package config
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
 	"time"
+)
+
+var newLogger = logger.New(
+	log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+	logger.Config{
+		SlowThreshold:             time.Second, // 慢 SQL 阈值
+		LogLevel:                  logger.Info, // 日志级别
+		IgnoreRecordNotFoundError: true,        // 忽略ErrRecordNotFound（记录未找到）错误
+		Colorful:                  false,       // 禁用彩色打印
+	},
 )
 
 func GetGlobalDB() (*gorm.DB, error) {
 	dsn := "root:123456@tcp(127.0.0.1:3306)/mydatabase?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 
 	// Get generic database object sql.DB to use its functions
 	sqlDB, err := db.DB()
